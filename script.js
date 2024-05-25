ca76eaf3582844c8d2c81560f8a8d7e9c912af52
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-25 21:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-document.querySelector("#form1").addEventListener("submit", function(event){
-  event.preventDefault(); // This line stops the form from submitting normally
-  let fname = document.querySelector('input[name="fname"]').value;
-  let lname = document.querySelector('input[name="lname"]').value;
-  alert(fname + " " + lname);
-});
+function getFormvalue(event) {
+    event.preventDefault(); // This line stops the form from submitting normally
+    var fname = document.querySelector('input[name="fname"]').value;
+    var lname = document.querySelector('input[name="lname"]').value;
+    alert(fname + " " + lname);
+}
+
+document.querySelector("#form1").addEventListener("submit", getFormvalue);
