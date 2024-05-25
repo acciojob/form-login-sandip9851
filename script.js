18826dc99c7fd78ed2fe3e79916cf3354f685541
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-25 21:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+document.querySelector("#form1").addEventListener("submit", function(event){
+  event.preventDefault(); // This line stops the form from submitting normally
+  let fname = document.querySelector('input[name="fname"]').value;
+  let lname = document.querySelector('input[name="lname"]').value;
+  alert(fname + " " + lname);
+});
